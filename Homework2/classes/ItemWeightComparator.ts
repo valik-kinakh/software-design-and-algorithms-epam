7d37d7f041750b654652aa57544b355da5877f3d
Valentyn_Kinakh <valentyn_kinakh@epam.com>
2022-10-12 16:27:10 +0300

    implemented ItemWeightComparator class

--- Homework2/classes/ItemWeightComparator.ts
+++ Homework2/classes/ItemWeightComparator.ts
@@ -3,6 +3,12 @@ import { ItemComparator } from './ItemComparator';
 
 export class ItemWeightComparator implements ItemComparator {
     public compare(first: Item, second: Item) {
-        // your code goes here
+        const firstWeight = first.getWeight();
+        const secondWeight = second.getWeight();
+
+        if (firstWeight > secondWeight) return 1;
+        else if (firstWeight < secondWeight) return -1;
+
+        return first.compareTo(second);
     }
 }
