4f68e014279666aca72fe3e31e6269c6479d42aa
Valentyn_Kinakh <valentyn_kinakh@epam.com>
2022-10-19 12:28:50 +0300

    implemented data converter

--- Homework3/src/App.tsx
+++ Homework3/src/App.tsx
@@ -5,24 +5,24 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import { Image, User, Account } from '../types';
 import { Table, Filters, Sort, Search, Row } from './components';
 import { getImages, getUsers, getAccounts } from './mocks/api';
-import rows from './mocks/rows.json';
+// import rows from './mocks/rows.json';
 
 import styles from './App.module.scss';
+import dataConverter from './helpers/dataConverter';
 
 // mockedData has to be replaced with parsed Promises’ data
-const mockedData: Row[] = rows.data;
+// const mockedData: Row[] = rows.data;
 
 export const App: FC = () => {
   const [data, setData] = useState<Row[]>(undefined);
 
   useEffect(() => {
     // fetching data from API
-    Promise.all([
-      getImages(),
-      getUsers(),
-      getAccounts(),
-    ]).then(([images, users, accounts]: [Image[], User[], Account[]]) =>
-      console.log(images, users, accounts)
+    Promise.all([getImages(), getUsers(), getAccounts()]).then(
+      ([images, users, accounts]: [Image[], User[], Account[]]) => {
+        const rows = dataConverter(users, accounts, images);
+        setData(rows);
+      }
     );
   }, []);
 
@@ -36,7 +36,7 @@ export const App: FC = () => {
           </div>
           <Search />
         </div>
-        <Table rows={data || mockedData} />
+        {data && <Table rows={data} />}
       </div>
     </StyledEngineProvider>
   );
---
+++ Homework3/src/helpers/dataConverter.ts
@@ -0,0 +1,52 @@
+import { Account, Image, User } from '../../types';
+import { Row } from '../components';
+
+const convertUserInfo = (user: User) => {
+  return {
+    username: user.username,
+    name: user.name,
+    country: user.country,
+  };
+};
+
+const convertAccountInfo = (account: Account) => {
+  const totalSum = account.payments.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.totalSum,
+    0
+  );
+  return {
+    lastPayments: totalSum,
+    posts: account.posts,
+  };
+};
+
+const convertImageInfo = (image: Image) => {
+  return {
+    avatar: image.url,
+  };
+};
+
+const dataConverter = (
+  users: User[],
+  accounts: Account[],
+  images: Image[]
+): Row[] => {
+  const rows = users.map((user, index): Row => {
+    const { lastPayments, posts } = convertAccountInfo(accounts[index]);
+    const { avatar } = convertImageInfo(images[index]);
+    const { username, name, country } = convertUserInfo(user);
+
+    return {
+      lastPayments,
+      posts,
+      avatar,
+      username,
+      name,
+      country,
+    };
+  });
+
+  return rows;
+};
+
+export default dataConverter;
