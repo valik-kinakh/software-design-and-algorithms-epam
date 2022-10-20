6eb4ca954abaecf9f0e31316f7a0eea16267f251
Valentyn_Kinakh <valentyn_kinakh@epam.com>
2022-10-20 13:09:28 +0300

    fixed using eat message

--- Homework2/classes/Consumable.ts
+++ Homework2/classes/Consumable.ts
@@ -1,44 +1,44 @@
 // your code goes here
 
-import {Item} from "./Item";
+import { Item } from "./Item";
 
 export abstract class Consumable extends Item {
-    protected consumed: boolean;
-    protected spoiled: boolean;
+  protected consumed: boolean;
+  protected spoiled: boolean;
 
-    // constructor
-    protected constructor(name: string, value: number, weight: number, spoiled: boolean) {
-        super(name, value, weight);
+  // constructor
+  protected constructor(
+    name: string,
+    value: number,
+    weight: number,
+    spoiled: boolean
+  ) {
+    super(name, value, weight);
 
-        this.spoiled = spoiled;
-        this.consumed = false;
+    this.spoiled = spoiled;
+    this.consumed = false;
+  }
+
+  //setters
+  public setConsumed(value: boolean): void {
+    this.consumed = value;
+  }
+
+  //methods
+  public use(): string | void {
+    if (!this.consumed && !this.spoiled) {
+      return this.eat();
     }
 
-    //setters
-    public setConsumed(value: boolean): void{
-        this.consumed = value;
+    const isBread: boolean = this.name === "bread";
+    if (isBread && this.consumed) {
+      return "There is nothing left of the bread to consume.";
     }
 
-    //methods
-    public use():string | void {
-        if (!this.consumed && !this.spoiled){
-            return this.eat();
-        }
+    return "The consumable is consumed or spoiled";
+  }
 
-        const isBread:boolean = this.name === "bread";
-        if (isBread && this.consumed){
-            return "There is nothing left of the bread to consume.";
-        }
-
-        return "The consumable is consumed or spoiled";
-    }
-
-    protected eat():string{
-        const isBread:boolean = this.name === "bread";
-        if (isBread){
-            return `You eat the bread.
-            ${this.spoiled && "You feel sick."}`;
-        }
-        return `You eat ${this.name}`;
-    }
+  protected eat(): string {
+    return `You eat ${this.name}`;
+  }
 }
