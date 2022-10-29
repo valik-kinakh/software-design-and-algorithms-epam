23ab11bfbaec5786b2e4b48c895604d07b84fa8c
valentyn-kinakh <valentyn_kinakh@epam.com>
2022-10-29 11:33:06 +0300

    fix mutable data

--- Homework3/src/helpers/sortData.ts
+++ Homework3/src/helpers/sortData.ts
@@ -1,12 +1,14 @@
 import { Row } from '../components';
 
 const sortData = (rows: Row[], sort: string) => {
+  const rowsCopy = [...rows];
+
   if (sort === 'desc') {
-    return rows.sort((row1, row2) => {
+    return rowsCopy.sort((row1, row2) => {
       return row2.lastPayments - row1.lastPayments;
     });
   }
-  return rows.sort((row1, row2) => {
+  return rowsCopy.sort((row1, row2) => {
     return row1.lastPayments - row2.lastPayments;
   });
 };
